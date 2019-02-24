4c972d9ce5f2b1786d0dd9361400e4a8f49b1f52
Julien LE THENO <julien.letheno@gmail.com>
2019-02-24 14:38:06 +0100

    implement widget rectangle and widget circle creation

--- winman/src/winman.h
+++ winman/src/winman.h
@@ -74,6 +74,44 @@ widget_line* create_widget_line(int posX, int posY, int posX2, int posY2, char r
  */
 widget_text* create_widget_text(int posX, int posY, char* text);
 
+/** Creates a 'rectangle' widget
+ *  @param posX   : the X position of the first point, relative to the window
+ *  @param posY   : the Y position of the first point, relative to the window
+ *  @param posX2  : the X position of the second point, relative to the window
+ *  @param posY2  : the Y position of the second point, relative to the window
+ *  @param rep    : the character representation of the rectangle, will be displayed on screen
+ *  @return the widget_rectangle created
+ */
+widget_rectangle* create_widget_rectangle(int posX, int posY, int posX2, int posY2, char rep);
+
+/** Creates a filled 'rectangle' widget
+ *  @param posX   : the X position of the first point, relative to the window
+ *  @param posY   : the Y position of the first point, relative to the window
+ *  @param posX2  : the X position of the second point, relative to the window
+ *  @param posY2  : the Y position of the second point, relative to the window
+ *  @param rep    : the character representation of the rectangle, will be displayed on screen
+ *  @return the widget_rectangle created
+ */
+widget_rectangle* create_widget_rectangle_filled(int posX, int posY, int posX2, int posY2, char rep);
+
+/** Creates a 'circle' widget
+ *  @param posX   : the X position of the center point, relative to the window
+ *  @param posY   : the Y position of the center point, relative to the window
+ *  @param radius : the radius of the circle, relative to the window
+ *  @param rep    : the character representation of the circle, will be displayed on screen
+ *  @return the widget_circle created
+ */
+widget_circle* create_widget_circle(int posX, int posY, int radius, char rep);
+
+/** Creates a filled 'circle' widget
+ *  @param posX   : the X position of the center point, relative to the window
+ *  @param posY   : the Y position of the center point, relative to the window
+ *  @param radius : the radius of the circle, relative to the window
+ *  @param rep    : the character representation of the circle, will be displayed on screen
+ *  @return the widget_circle created
+ */
+widget_circle* create_widget_circle_filled(int posX, int posY, int radius, char rep);
+
 
 /** Creates a generic widget from a specific widget
  *  @param type        : the type of the widget (see widget_type_enum)

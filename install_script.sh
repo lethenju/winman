43a321bb5bd948675055d97d2cd96c24adeceab3
Julien LE THENO <julien.letheno@gmail.com>
2020-05-06 17:07:12 +0200

    [Up] Build dependencies

--- install_script.sh
+++ install_script.sh
@@ -7,33 +7,15 @@ echo "== Install Winman =="
 
 echo "== Dep -> Clone Termlib =="
 git clone git@github.com:lethenju/termlib.git
-
 cd termlib
 
 echo "== Dep -> Clone and install Resman =="
-git clone git@github.com:lethenju/resman.git
-cd resman
-mkdir build
-cd build
-cmake ..
-sudo make install
-cd ../..
+git clone git@github.com:lethenju/resman.github
 
-echo "== Dep -> Clone and install LogSystem =="
+echo "== Dep -> Clone LogSystem =="
 git clone git@github.com:lethenju/log_system.git
-cd log_system
-mkdir build
-cd build
-cmake ..
-sudo make install
-cd ../..
 
-echo "== Dep -> Install Termlib"
-mkdir build
-cd build
-cmake ..
-sudo make install
-cd ../..
+cd ..
 
 echo "== Install winman"
 mkdir build 

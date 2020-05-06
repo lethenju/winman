187b75bad68ff519e4856e37114ad42aeab2db6d
Julien LE THENO <julien.letheno@gmail.com>
2020-05-06 16:43:07 +0200

    [Up] Install script automatically builds everything

--- install_script.sh
+++ install_script.sh
@@ -5,15 +5,38 @@
 
 echo "== Install Winman =="
 
-echo "== Dep -> Install Termlib =="
+echo "== Dep -> Clone Termlib =="
 git clone git@github.com:lethenju/termlib.git
 
 cd termlib
 
-echo "== Dep -> Install LogSystem =="
-git clone git@github.com:lethenju/log_system.git
-
-
-
-echo "== Dep -> Install Resman =="
+echo "== Dep -> Clone and install Resman =="
 git clone git@github.com:lethenju/resman.git
+cd resman
+mkdir build
+cd build
+cmake ..
+sudo make install
+cd ../..
+
+echo "== Dep -> Clone and install LogSystem =="
+git clone git@github.com:lethenju/log_system.git
+cd log_system
+mkdir build
+cd build
+cmake ..
+sudo make install
+cd ../..
+
+echo "== Dep -> Install Termlib"
+mkdir build
+cd build
+cmake ..
+sudo make install
+cd ../..
+
+echo "== Install winman"
+mkdir build 
+cd build
+cmake ..
+sudo make install
